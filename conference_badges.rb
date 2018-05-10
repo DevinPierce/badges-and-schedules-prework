6f43d532582e11fba0547cdb9c248893f99c3038
Devin Pierce <devinmpierce@gmail.com>
2018-05-10 00:01:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badgeArray = []
+  for name in array do
+    badgeArray << badge_maker(name)
+  end
+  badgeArray
+end
